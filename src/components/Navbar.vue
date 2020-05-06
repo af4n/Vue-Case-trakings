5836584c78d90c7e1d06fdcb98ea3e7b8b2cb946
Aleksey Afanaskin <aleksey.afanaskin@gmail.com>
2020-05-07 00:46:36 +0300

    page create and list was added

---
+++ src/components/Navbar.vue
@@ -0,0 +1,38 @@
+<template>
+  <nav class="blue darken-3">
+    <div class="nav-wrapper">
+      <router-link to="/" class="brand-logo">Tasks</router-link>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <router-link
+          tag="li"
+          to="/"
+          exact
+          active-class="active"
+        >
+          <a href="#">Create</a>
+        </router-link>
+        <router-link
+          tag="li"
+          to="/list"
+          exact
+          active-class="active"
+        >
+          <a href="#">List</a>
+        </router-link>
+
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+  nav {
+    padding: 0 2rem;
+  }
+</style>
---
+++ src/views/Task.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2>Task</h2>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
