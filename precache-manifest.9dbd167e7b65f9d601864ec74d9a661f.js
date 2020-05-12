c06d5972a000dbd02f5e1c2988b456960928bcfb
Aleksey Afanaskin <aleksey.afanaskin@gmail.com>
2020-05-12 23:00:10 +0300

    Initial dist subtree commit

---
+++ precache-manifest.9dbd167e7b65f9d601864ec74d9a661f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fcf580799e2308ea23d",
+    "url": "/css/app.bdb17bb2.css"
+  },
+  {
+    "revision": "4e94df55827c45808e2a",
+    "url": "/css/chunk-045223a6.90cbd604.css"
+  },
+  {
+    "revision": "d7a78530668bed309ded",
+    "url": "/css/chunk-vendors.3b8adcd8.css"
+  },
+  {
+    "revision": "4d896bd746f99cd662df8f66d2a48829",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8fcf580799e2308ea23d",
+    "url": "/js/app.fbfb40bf.js"
+  },
+  {
+    "revision": "4e94df55827c45808e2a",
+    "url": "/js/chunk-045223a6.cbffa509.js"
+  },
+  {
+    "revision": "daa601cb7f487d7c8d53",
+    "url": "/js/chunk-2d0b64c8.13a9f9fa.js"
+  },
+  {
+    "revision": "d7a78530668bed309ded",
+    "url": "/js/chunk-vendors.c207e279.js"
+  },
+  {
+    "revision": "33596049e9f2532f2d5901af1172f3aa",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
